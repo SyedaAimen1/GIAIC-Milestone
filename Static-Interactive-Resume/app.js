a05e42f0a7888dccb5db216603c1e142b99e5585
SyedaAimen1 <syedaaimen24@gmail.com>
2025-02-08 23:51:50 +0500

    ok All

---
+++ Static-Interactive-Resume/app.js
@@ -0,0 +1,14 @@
+// Get the switch and skills section elements
+var skillsSwitch = document.getElementById("skills-switch");
+var skillsSection = document.getElementById("skills");
+// Initially hide the skills section (optional)
+skillsSection.style.display = "block";
+// Handle switch toggle
+skillsSwitch.addEventListener("change", function () {
+    if (skillsSwitch.checked) {
+        skillsSection.style.display = "block"; // Show the skills section
+    }
+    else {
+        skillsSection.style.display = "none"; // Hide the skills section
+    }
+});
